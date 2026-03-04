c85d8c5231ab808aefa3f138c579fbafb63d1cc1
nick-yay <nicolasludugerio@gmail.com>
2026-03-04 20:32:23 -0300

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/com/libraryflow/libraryflow/LibraryflowApplication.java
@@ -0,0 +1,14 @@
+package com.libraryflow.libraryflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryflowApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/libraryflow/libraryflow/LibraryflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryflow.libraryflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
